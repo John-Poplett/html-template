8642b76acb77b34663a6aab8043a2277abdf89bc
vcdt34 <john.poplett@motorola.com>
2011-11-10 15:25:11 -0600

    Initial commit
    Signed-off-by: John Poplett <john.poplett@acm.org>

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject html-template "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :aot [html-template.core]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [fnparse "2.2.8-SNAPSHOT"]])
---
+++ test/html_template/test/core.clj
@@ -0,0 +1,6 @@
+(ns html-template.test.core
+  (:use [html-template.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
